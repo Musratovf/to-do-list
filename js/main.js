262c77de55eb7e5efc382f98333404666cddd85b
Musratov <musratof@gmail.com>
2022-02-06 07:34:11 +0500

    add showtask function

--- js/main.js
+++ js/main.js
@@ -24,3 +24,26 @@ headerBtn.onclick = () => {
     localStorage.setItem("New Todo", JSON.stringify(listArr));
     showTasks()
 };
+
+function showTasks() {
+    let getLocalStorage = localStorage.getItem("New Todo");
+    if(getLocalStorage == null) {
+        listArr = [];
+    }else {
+        listArr = JSON.parse(getLocalStorage);
+    } 
+    const pendingNumber = document.querySelector('.pending__number'); 
+    pendingNumber.textContent = listArr.length;
+    if(listArr.length > 0) {
+        deleteAllBtn.classList.add("active")
+    }else {
+        deleteAllBtn.classList.remove("active")
+
+    }
+    let newLiTag = ''
+    listArr.forEach((element, index) => {
+        newLiTag += <li class="todolist__item">${element}<span onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>
+    });
+    todoList.innerHTML = newLiTag;
+    elInput.value = null;
+}
