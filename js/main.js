64ac6b3cdb3dc7bea6694ffc91dd0464a4def1ad
Musratov <musratof@gmail.com>
2022-02-06 07:32:10 +0500

    header btn add

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ const elInput = document.querySelector('.header__input');
 const headerBtn = document.querySelector('.header__btn');
 const todoList = document.querySelector('.todolist');   
 const deleteAllBtn = document.querySelector('.footer__btn');   
+
+elInput.onkeyup = () => {
+    let userData = elInput.value;
+    if(userData.trim() != 0 ) {
+        headerBtn.classList.add("active")
+    }else {
+        headerBtn.classList.remove("active")
+    }
+}
