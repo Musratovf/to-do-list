73ec14a3df578cd984a717f96ab509a7f11aecc0
Musratov <musratof@gmail.com>
2022-02-06 07:31:24 +0500

    add js const

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const elInput = document.querySelector('.header__input');
+const headerBtn = document.querySelector('.header__btn');
+const todoList = document.querySelector('.todolist');   
+const deleteAllBtn = document.querySelector('.footer__btn');   
